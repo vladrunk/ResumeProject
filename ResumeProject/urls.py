c0d3b67a86602213ea656fa52aadf5a52007514a
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-02-03 03:45:59 +0200

    add i18n redirect

--- ResumeProject/urls.py
+++ ResumeProject/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = [
-    path('', include('main_page.urls')),
     path('god/', admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('main_page.urls')),
+    prefix_default_language=False,
+)
--- main_page/views.py
+++ main_page/views.py
@@ -1,28 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from ResumeProject import settings
 from .models import User
 
 
 def index(req):
-    user = User.objects.filter(id=1)[0]
-    if user:
-        context = {
-            'browse_lang': req.LANGUAGE_CODE,
-            'profile_image': user.avatar.url,
-            'fio': f'{user.first_name} {user.last_name}',
-            'specs': user.specialization,
-            'phone': user.phone,
-            'email': user.email,
-            'tg': user.tg,
-            'inst': user.inst,
-            'linkedin': user.linkedin,
-            'git': user.git,
-            'about': user.about,
-            'work_exp': user.experience,
-            'skills': user.skill,
-            'edus': user.education,
-            'langs': user.language,
-            'musics': user.music,
-        }
+    lang = req.COOKIES.get('lang')
+    if not lang:
+        lang = req.LANGUAGE_CODE
+    redirect_url = f'/{lang}/' if lang != settings.LANGUAGE_CODE else '/'
+    if req.path != redirect_url:
+        response = redirect(to=redirect_url)
     else:
-        context = {}
-    return render(req, 'main_page/templates/index.html', context)
+        user = User.objects.filter(id=1)[0]
+        if user:
+            context = {
+                'browse_lang': req.LANGUAGE_CODE,
+                'profile_image': user.avatar.url,
+                'fio': f'{user.first_name} {user.last_name}',
+                'specs': user.specialization,
+                'phone': user.phone,
+                'email': user.email,
+                'tg': user.tg,
+                'inst': user.inst,
+                'linkedin': user.linkedin,
+                'git': user.git,
+                'about': user.about,
+                'work_exp': user.experience,
+                'skills': user.skill,
+                'edus': user.education,
+                'langs': user.language,
+                'musics': user.music,
+            }
+        else:
+            context = {}
+        response = render(req, 'main_page/templates/index.html', context)
+    response.set_cookie(key='lang', value=lang, samesite='Lax', max_age=60*60*24*31, path='/', )
+    return response
